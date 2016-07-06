926d4033a62a019e4c7d8d7f0ebd026d9c15bc6f
ryancortez <ryancortez@mac.com>
2016-07-06 12:00:28 -0500

    Initial Commit

---
+++ Pet Store/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pet Store
+//
+//  Created by Ryan Cortez on 7/6/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
